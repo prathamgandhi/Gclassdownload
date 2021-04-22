3061a9c2ce7658173d50a1f6901a05cb04829082
prathamgandhi <76698421+prathamgandhi@users.noreply.github.com>
2021-04-22 21:00:22 +0530

    Update drive_download.py

--- drive_download.py
+++ drive_download.py
@@ -1,3 +1,6 @@
+"""
+This python code is used for downloading files from google drive given the proper authentication
+"""
 import os.path
 import io
 from googleapiclient.http import MediaIoBaseDownload
@@ -5,13 +8,13 @@ from googleapiclient.http import MediaIoBaseDownload
 
 def drive_dl(drive_service, file_id, path):
     request = drive_service.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
+    fh = io.BytesIO()        # fh is a byte_based buffer to which the file will be downloaded before writing to the system. The buffer stores the file in RAM until it is written to disk
+    downloader = MediaIoBaseDownload(fh, request)   # A download object which is used to download files to the above byte buffer
     done = False
     while done is False:
         status, done = downloader.next_chunk()
         print("Download %d%%." % int(status.progress() * 100))
     fh.seek(0)
-    with open(os.path.join(path), 'wb') as f:
+    with open(os.path.join(path), 'wb') as f:   #writing from the byte buffer to the disk
         f.write(fh.read())
         f.close()
